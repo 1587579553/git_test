a988ac24e31374c4e423d8bd7d31d5b67d0198ce
moolee <moolee@163.com>
2022-09-21 20:58:47 +0800

    git-test_third

--- git_test/git_demo_one.py
+++ git_test/git_demo_one.py
@@ -1,2 +1,4 @@
 for i in range(10):
     print("I love python more than 3000 times")
+    print("can you tell me why the teacher taught me is easy,but we learn is difficult")
+    print("I am tire,pls let me fly from the tallest floor")
