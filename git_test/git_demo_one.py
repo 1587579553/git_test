fc63e5631993c618d200a1dc3a55aa36f519935b
1587579553 <79459552+1587579553@users.noreply.github.com>
2022-09-25 10:23:46 +0800

    git-demo_one modified

--- git_test/git_demo_one.py
+++ git_test/git_demo_one.py
@@ -3,4 +3,5 @@ for i in range(10):
     print("I dont't konw what I am doing now.")
     print("can you tell me why the teacher taught me is easy,but we learn is difficult")
     print("I am tire,pls let me fly from the tallest floor")
-print("today is wendsday")
+    print("today is wendsday")
+    print("today is sunday!I fell so ")
